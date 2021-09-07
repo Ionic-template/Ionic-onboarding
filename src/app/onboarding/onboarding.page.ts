a8969c93b91e62150c052cc1fd4ff8282d920aef
khan <khan@khans-iMac.local>
2021-09-08 00:42:10 +0530

    Base layout.

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -9,6 +9,30 @@ export class OnboardingPage implements OnInit {
 
   constructor() { }
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+
+  };
+
+  appInto = [
+    {
+      title: 'Welcome to Soundsfluent',
+      text: 'Close the English fluency gap by the 10% that stand between you just functioning and thriving.',
+      img: 'onboard-1.svg'
+    },
+    {
+      title: 'Welcome to Soundsfluent',
+      text: 'Close the English fluency gap by the 10% that stand between you just functioning and thriving.',
+      img: 'onboard-1.svg'
+    },
+    {
+      title: 'Welcome to Soundsfluent',
+      text: 'Close the English fluency gap by the 10% that stand between you just functioning and thriving.',
+      img: 'onboard-1.svg'
+    },
+  ]
+
   ngOnInit() {
   }
 
